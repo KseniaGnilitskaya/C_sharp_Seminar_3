64f4b94b6545f0db0f8b837096923921a26ac5a9
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-23 22:49:46 +0300

    Task 4 added

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -7,10 +7,21 @@
 //    i+=1;
 //}
 
-Console.Write("Enter the number: ");
-int number = int.Parse(Console.ReadLine());
-Console.Write("Squares of number: ");
+//Console.Write("Enter the number: ");
+//int number = int.Parse(Console.ReadLine());
+//Console.Write("Squares of number: ");
+//
+//for (int i = 0, i < number, i++) {
+//    Console.Write(Math.Pow(i+1, 2));
+//}
 
-for (int i = 0, i < number, i++) {
-    Console.Write(Math.Pow(i+1, 2));
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] squares = new int[number];  // решение через массивы
+Console.Write("Таблица квадратов: ");
+for (int i = 0; i < number; i++){
+    squares[i] = (i + 1)*(i + 1);
+    Console.Write($"{squares[i]} ");
 }
