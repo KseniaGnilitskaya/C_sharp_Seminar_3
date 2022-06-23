1f695bcd0715dbaa14a3256908d1a012cb96d9ef
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-23 22:43:07 +0300

    Task 4 updated

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,8 +1,16 @@
-﻿Console.Write("Enter the number: ");
-int number = int.Parse(Console.ReadLine());
-int i = 1;
+﻿//Console.Write("Enter the number: ");
+//int number = int.Parse(Console.ReadLine());
+//int i = 1;
+//
+//while (i <= number){
+//    Console.WriteLine(i * i);
+//    i+=1;
+//}
 
-while (i <= number){
-    Console.WriteLine(i * i);
-    i+=1;
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Squares of number: ");
+
+for (int i = 0, i < number, i++) {
+    Console.Write(Math.Pow(i+1, 2));
 }
