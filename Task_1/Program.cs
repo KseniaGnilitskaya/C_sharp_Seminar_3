678fe83873d63c8b96e78bfb68dd4679c6d5af59
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-23 19:47:37 +0300

    Task 1 updated

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] coordinate = new int [2]; // new int [2] - это две координаты х и у
+
+Console.Write("Enter coordinate X: ");
+coordinate[0] = int.Parse(Console.ReadLine());
+
+Console.Write("Enter coordinate Y: ");
+coordinate[1] = int.Parse(Console.ReadLine());
+
+if (coordinate[0] > 0 & coordinate[1] > 0) {
+    Console.Write("It's the first quater");
+}
+if (coordinate[0] < 0 & coordinate[1] > 0) {
+    Console.Write("It's the second quater");
+}
+if (coordinate[0] < 0 & coordinate[1] < 0) {
+    Console.Write("It's the third quater");
+}
+if (coordinate[0] > 0 & coordinate[1] < 0) {
+    Console.Write("It's the forth quater");
+}
