8384f3cf4d1a4420a0914777dc20fc28e40bcd57
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-23 20:14:57 +0300

    Task 2 added

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter the number from 1 to 4: ");
+int quarter = int.Parse(Console.ReadLine());
+
+
+if (quarter == 1) {
+    Console.WriteLine("x > 0, y > 0");
+} 
+else if (quarter == 2) {
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quarter == 3) {
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (quarter == 4) {
+    Console.WriteLine("x > 0, y < 0");
+}
